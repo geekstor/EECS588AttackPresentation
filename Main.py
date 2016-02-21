96e4f6e19e6d17473e7a34d320d4571f587e2691
Valliappa Chockalingam <valli@umich.edu>
2016-02-21 12:03:43 -0500

    Loading of DAE working. CaptionNet also seems to be working.

--- Main.py
+++ Main.py
@@ -10,12 +10,10 @@ for possibledir in os.listdir(path):
         if os.path.isdir(os.path.join(path, possibledir)):
             contents_of_dir = [file for file in os.listdir(os.path.join(path, possibledir))]
             if "control.txt" in contents_of_dir:
-                labeled_samples[possibledir] = [file for file in contents_of_dir if ".png" in file or ".jpeg" in file or ".jpg" in file]
+                labeled_samples[possibledir] = [file for file in contents_of_dir if ".png" in file or ".jpeg" in file or ".jpg" in file or ".gif" in file]
             else:
-                unlabeled_samples[possibledir] = [file for file in contents_of_dir if ".png" in file or ".jpeg" in file or ".jpg" in file]
+                unlabeled_samples[possibledir] = [file for file in contents_of_dir if ".png" in file or ".jpeg" in file or ".jpg" in file or ".gif" in file]
 
-
-print(labeled_samples)
 
 for k, v in labeled_samples.items():
     f = open(path + "/" + k + "/control.txt")
@@ -24,11 +22,18 @@ for k, v in labeled_samples.items():
     labeled_samples[k] = zip(labeled_samples[k], answers)
 
 from scipy import misc
+from scipy.ndimage import imread
 
 for k in labeled_samples:
     for i in range(len(list(labeled_samples[k]))):
-        labeled_samples[k][i] = tuple([misc.imread(path + "/" + k + "/" + labeled_samples[k][i][0]),
+        labeled_samples[k][i] = tuple([misc.imresize(imread(path + "/" + k + "/" + labeled_samples[k][i][0], mode='RGB'),
+                                        size=(128, 16)).flatten()/float(256.0),
                                         labeled_samples[k][i][1]])
+
+for k in unlabeled_samples:
+    for i in range(len(list(unlabeled_samples[k]))):
+        unlabeled_samples[k][i] = misc.imresize(imread(path + "/" + k + "/" + unlabeled_samples[k][i], mode='RGB'),
+                                size=(128, 16)).flatten()/float(256.0)
 
 # For displaying captcha
 
@@ -39,4 +44,50 @@ for k in labeled_samples:
 # print labeled_samples['xanga'][72][1]
 # plt.show()
 
+import numpy
+import random
+import sys
 
+rand_seed_value = 0
+
+def get_batch(size, partial_answer=False, step=False):
+    img_list = []
+    answer_list = []
+    partial_answer_list = []
+    rest_answer_list = []
+    global rand_seed_value
+    random.seed(rand_seed_value)
+    for i in range(size):
+        rand_key = labeled_samples.keys()[random.randint(0, len(labeled_samples)) - 1]
+        rand_ind = random.randint(0, len(list(labeled_samples[rand_key])) - 1)
+        img_list.append(labeled_samples[rand_key][rand_ind][0])
+        if not partial_answer:
+            answer_list.append(labeled_samples[rand_key][rand_ind][1])
+        else:
+            rand_end_char = random.randint(0, len(list(labeled_samples[rand_key][rand_ind][1] - 1)))
+            partial_answer_list.append(labeled_samples[rand_key][rand_ind][1][:rand_end_char])
+            rest_answer_list.append(labeled_samples[rand_key][rand_ind][1][rand_end_char:])
+
+    if step:
+        rand_seed_value = random.randint(0, sys.maxint)
+
+    if not partial_answer:
+        return [numpy.array(img_list, dtype=float), answer_list]
+    else:
+        return [numpy.array(img_list, dtype=float), partial_answer_list, rest_answer_list]
+
+def get_test_batch(size):
+    img_list = []
+    for i in range(size):
+        rand_key = unlabeled_samples.keys()[numpy.random.randint(low=0, high=len(unlabeled_samples))]
+        rand_ind = numpy.random.randint(low=0, high=len(list(unlabeled_samples[rand_key])))
+        img_list.append(unlabeled_samples[rand_key][rand_ind])
+    return numpy.array(img_list, dtype=float), numpy.array(img_list, dtype=float)
+#
+# def get_sample():
+#     global labeled_samples
+#     labeled_samples = labeled_samples
+#     while True:
+#         rand_key = labeled_samples.keys()[numpy.random.randint(low=0, high=len(labeled_samples))]
+#         rand_ind = numpy.random.randint(low=0, high=len(list(labeled_samples[rand_key])))
+#         yield numpy.array(labeled_samples[rand_key][rand_ind][0]).reshape(1, 2048*3), numpy.array(labeled_samples[rand_key][rand_ind][0]).reshape(1, 2048*3)
--- NeuralNet.py
+++ NeuralNet.py
@@ -70,46 +70,59 @@ import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
-from keras.
+from keras.layers import Activation
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.layers import Merge
+from keras.layers import TimeDistributedDense
+from keras.layers import Embedding
+from keras.layers import GRU
+from keras.layers import RepeatVector
+from keras.layers import GaussianNoise
+from keras.layers import BatchNormalization
+from keras.layers import containers
+from keras.layers import AutoEncoder
+from keras.optimizers import Adagrad
+from keras.models import model_from_json
 
-max_caption_len = 16
-vocab_size = 10000
+max_caption_len = 10
+vocab_size = 62
 
 # first, let's define an image model that
 # will encode pictures into 128-dimensional vectors.
 # it should be initialized with pre-trained weights.
-image_model = Sequential()
-image_model.add(Convolution2D(32, 3, 3, border_mode='valid', input_shape=(3, 100, 100)))
-image_model.add(Activation('relu'))
-image_model.add(Convolution2D(32, 3, 3))
-image_model.add(Activation('relu'))
-image_model.add(MaxPooling2D(pool_size=(2, 2)))
-
-image_model.add(Convolution2D(64, 3, 3, border_mode='valid'))
-image_model.add(Activation('relu'))
-image_model.add(Convolution2D(64, 3, 3))
-image_model.add(Activation('relu'))
-image_model.add(MaxPooling2D(pool_size=(2, 2)))
-
-image_model.add(Flatten())
-image_model.add(Dense(128))
-
+# ae_model = Sequential()
+# ae_model.add(GaussianNoise(sigma=0.05, batch_input_shape=(100, 2048*3)))
+# ae_model.add(BatchNormalization())
+# encoder = containers.Sequential([Dense(input_dim=2048*3, output_dim=1024*3, activation='sigmoid'), BatchNormalization(), Dense(input_dim=1024*3, output_dim=512*3, activation='sigmoid'), BatchNormalization()])
+# decoder = containers.Sequential([Dense(input_dim=512*3, output_dim=1024*3, activation='sigmoid'), BatchNormalization(), Dense(input_dim=1024*3, output_dim=2048*3, activation='sigmoid')])
+# ae_model.add(AutoEncoder(encoder=encoder, decoder=decoder, output_reconstruction=True))
 # let's load the weights from a save file.
-image_model.load_weights('weight_file.h5')
+
+ae_model = model_from_json(open('./Graph/dAE_arch.json').read())
+ae_model.load_weights('./Graph/dAE_weights.h5')
+
+print(ae_model.layers)
+
+ae_model.layers.remove(0)
+
+for i in range(6):
+    ae_model.layers.remove(len(ae_model.layers))
 
 # next, let's define a RNN model that encodes sequences of words
 # into sequences of 128-dimensional word vectors.
 language_model = Sequential()
-language_model.add(Embedding(vocab_size, 256, input_length=max_caption_len))
+language_model.add(Embedding(vocab_size, 512, input_length=max_caption_len))
 language_model.add(GRU(output_dim=128, return_sequences=True))
 language_model.add(TimeDistributedDense(128))
 
 # let's repeat the image vector to turn it into a sequence.
-image_model.add(RepeatVector(max_caption_len))
+ae_model.add(RepeatVector(max_caption_len))
 
 # the output of both models will be tensors of shape (samples, max_caption_len, 128).
 # let's concatenate these 2 vector sequences.
-model = Merge([image_model, language_model], mode='concat', concat_axis=-1)
+model = Sequential()
+model.add(Merge([ae_model, language_model], mode='concat', concat_axis=-1))
 # let's encode this vector sequence into a single vector
 model.add(GRU(256, 256, return_sequences=False))
 # which will be used to compute a probability
@@ -119,10 +132,15 @@ model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
-# "images" is a numpy float array of shape (nb_samples, nb_channels=3, width, height).
-# "captions" is a numpy integer array of shape (nb_samples, max_caption_len)
-# containing word index sequences representing partial captions.
-# "next_words" is a numpy float array of shape (nb_samples, vocab_size)
-# containing a categorical encoding (0s and 1s) of the next word in the corresponding
-# partial caption.
-model.fit([images, partial_captions], next_words, batch_size=16, nb_epoch=100)
+
+
+# model.fit([images, partial_captions], next_words, batch_size=16, nb_epoch=100)
+
+
+import Main
+from keras.callbacks import TensorBoard
+from keras.callbacks import EarlyStopping
+ae_model.fit([Main.get_batch(5, True)[0], Main.get_batch(5, True)[1]], Main.get_batch(5, True, True)[2], nb_epoch=20000, verbose=1, batch_size=5,
+             show_accuracy=True, shuffle=True, #validation_data=Main.get_test_batch(5),
+             callbacks=[TensorBoard(log_dir='./Graph', histogram_freq=0),
+             EarlyStopping(verbose=True, monitor='val_loss', patience=50)])
