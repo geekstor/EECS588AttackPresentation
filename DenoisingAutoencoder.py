07e740ff8e2ce50fde600970caae42d1937a6fa7
geekstor <valli@umich.edu>
2016-02-22 19:13:55 -0500

    Modified Python Files.

---
+++ DenoisingAutoencoder.py
@@ -0,0 +1,43 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import GaussianNoise
+from keras.optimizers import Adagrad
+from keras.callbacks import TensorBoard
+from keras.layers import BatchNormalization
+from keras.callbacks import EarlyStopping
+from keras.layers import Activation
+
+ae_model = Sequential()
+ae_model.add(GaussianNoise(sigma=0.05, batch_input_shape=(5, 2048*3)))
+ae_model.add(BatchNormalization())
+
+ae_model.add(Dense(input_dim=2048*3, output_dim=1024*3))
+ae_model.add(BatchNormalization())
+ae_model.add(Activation('sigmoid'))
+ae_model.add(Dense(input_dim=1024*3, output_dim=512*3))
+ae_model.add(BatchNormalization())
+ae_model.add(Activation('sigmoid'))
+
+ae_model.add(Dense(input_dim=512*3, output_dim=1024*3))
+ae_model.add(BatchNormalization())
+ae_model.add(Activation('sigmoid'))
+ae_model.add(Dense(input_dim=1024*3, output_dim=2048*3))
+
+# encoder = containers.Sequential([Dense(input_dim=2048*3, output_dim=1024*3, activation='sigmoid'), Dense(input_dim=1024*3, output_dim=512*3, activation='sigmoid')])
+# decoder = containers.Sequential([Dense(input_dim=512*3, output_dim=1024*3, activation='sigmoid'), Dense(input_dim=1024*3, output_dim=2048*3, activation='sigmoid')])
+ae_model.compile(loss='mean_squared_error', optimizer=Adagrad(lr=0.1))
+
+import Main
+
+# for i in range(10000):
+#    print("Iter " + str(i) + " :" + str(ae_model.train_on_batch(X=Main.get_batch()[0], y = Main.get_batch()[0], accuracy=True)))
+# ae_model.fit_generator(Main.get_sample(), samples_per_epoch=100, nb_epoch=1000, show_accuracy=True, callbacks=[TensorBoard(log_dir='./Graph', histogram_freq=0)], nb_worker=1)
+
+ae_model.fit(Main.get_batch(100)[0], Main.get_batch(100, step=True)[0], nb_epoch=20000, verbose=1, batch_size=5,
+             show_accuracy=True, shuffle=True, validation_data=Main.get_test_batch(5),
+             callbacks=[TensorBoard(log_dir='./Graph', histogram_freq=0),
+             EarlyStopping(verbose=True, monitor='val_loss', patience=50)])
+
+json_string = ae_model.to_json()
+open('./Graph/dAE_arch.json', 'w').write(json_string)
+ae_model.save_weights(filepath='./Graph/dAE_weights.h5', overwrite=True)
--- Main.py
+++ Main.py
@@ -2,6 +2,9 @@ import os
 
 path = 'captcha-images'
 
+vocab_size = 62 + 1
+max_caption_len = 10
+
 labeled_samples = {}
 unlabeled_samples = {}
 
@@ -10,12 +13,10 @@ for possibledir in os.listdir(path):
         if os.path.isdir(os.path.join(path, possibledir)):
             contents_of_dir = [file for file in os.listdir(os.path.join(path, possibledir))]
             if "control.txt" in contents_of_dir:
-                labeled_samples[possibledir] = [file for file in contents_of_dir if ".gif" in file or ".png" in file or ".jpeg" in file or ".jpg" in file]
+                labeled_samples[possibledir] = [file for file in contents_of_dir if ".png" in file or ".jpeg" in file or ".jpg" in file or ".gif" in file]
             else:
-                unlabeled_samples[possibledir] = [file for file in contents_of_dir if ".gif" in file or ".png" in file or ".jpeg" in file or ".jpg" in file]
+                unlabeled_samples[possibledir] = [file for file in contents_of_dir if ".png" in file or ".jpeg" in file or ".jpg" in file or ".gif" in file]
 
-
-print(labeled_samples)
 
 for k, v in labeled_samples.items():
     f = open(path + "/" + k + "/control.txt")
@@ -24,54 +25,18 @@ for k, v in labeled_samples.items():
     labeled_samples[k] = zip(labeled_samples[k], answers)
 
 from scipy import misc
+from scipy.ndimage import imread
 
 for k in labeled_samples:
     for i in range(len(list(labeled_samples[k]))):
-        labeled_samples[k][i] = tuple([misc.imread(path + "/" + k + "/" + labeled_samples[k][i][0]),
+        labeled_samples[k][i] = tuple([misc.imresize(imread(path + "/" + k + "/" + labeled_samples[k][i][0], mode='RGB'),
+                                        size=(128, 16)).flatten()/float(256.0),
                                         labeled_samples[k][i][1]])
 
-
-# Generate training examples from key if provided, otherwise pick randomly among keys
-from random import randint
-def get_train_batch(batch_size, key = None):
-    keys = []
-    images = []
-    answers = []
-
-    key_names = labeled_samples.keys()
-    
-    if key == None or key not in key_names:
-        num_keys = len(labeled_samples)
-
-        for i in range(0, batch_size):
-            key = key_names[randint(0, num_keys-1)]
-            keys.append(key)
-            num_images = len(labeled_samples[key])
-
-            image_ind = randint(0, num_images-1)
-            image = labeled_samples[key][image_ind]
-            images.append(image[0])
-            answers.append(image[1])
-    else:
-        num_images = len(labeled_samples[key])
-        for i in range(0, batch_size):
-            keys.append(key)
-            image_ind = randint(0, num_images-1)
-            image = labeled_samples[key][image_ind]
-            images.append(image[0])
-            answers.append(image[1])
-
-    return (keys, images, answers)
-keys, images, answers = get_train_batch(50)
-
-# unsure what exactly you wanted to be different about test batch, right now just
-# returning images without answers
-def get_test_batch(batch_size, key = None):
-    keys, images, answers = get_train_batch(batch_size)
-    return (keys, images)
-
-
-
+for k in unlabeled_samples:
+    for i in range(len(list(unlabeled_samples[k]))):
+        unlabeled_samples[k][i] = misc.imresize(imread(path + "/" + k + "/" + unlabeled_samples[k][i], mode='RGB'),
+                                size=(128, 16)).flatten()/float(256.0)
 
 # For displaying captcha
 
@@ -82,4 +47,71 @@ def get_test_batch(batch_size, key = None):
 # print labeled_samples['xanga'][72][1]
 # plt.show()
 
+import numpy
+import random
+import sys
+import string
 
+rand_seed_value = 0
+
+mapping = {}
+
+idx = 0
+for c in string.digits:
+    mapping[c] = ord(c) - 48
+
+idx += len(string.digits)
+
+for c in string.uppercase:
+    mapping[c] = ord(c) - 65 + idx
+
+idx += len(string.uppercase)
+
+for c in string.ascii_lowercase:
+    mapping[c] = ord(c) - 97 + idx
+
+
+def get_batch(size, partial_answer=False, step=False):
+    img_list = []
+    answer_list = []
+    partial_answer_list = []
+    rest_answer_list = []
+    global rand_seed_value
+    random.seed(rand_seed_value)
+    for i in range(size):
+        rand_key = labeled_samples.keys()[random.randint(0, len(labeled_samples)) - 1]
+        rand_ind = random.randint(0, len(list(labeled_samples[rand_key])) - 1)
+        img_list.append(labeled_samples[rand_key][rand_ind][0])
+        if not partial_answer:
+            answer_list.append(labeled_samples[rand_key][rand_ind][1])
+        else:
+            rand_end_char = random.randint(0, len(labeled_samples[rand_key][rand_ind][1]) - 1)
+            partial_answer_t = [len(mapping.keys())] * (max_caption_len - rand_end_char)
+            partial_answer_t = partial_answer_t + [mapping[c]
+                                        for c in labeled_samples[rand_key][rand_ind][1][:rand_end_char]]
+            partial_answer_list.append(partial_answer_t)
+            rest_answer_list.append(numpy.eye(vocab_size)[mapping[labeled_samples[rand_key][rand_ind][1][rand_end_char]]])
+
+    if step:
+        rand_seed_value = random.randint(0, sys.maxint)
+
+    if not partial_answer:
+        return [numpy.array(img_list, dtype=float), answer_list]
+    else:
+        return [numpy.array(img_list, dtype=float), numpy.array(partial_answer_list), numpy.array(rest_answer_list)]
+
+def get_test_batch(size):
+    img_list = []
+    for i in range(size):
+        rand_key = unlabeled_samples.keys()[numpy.random.randint(low=0, high=len(unlabeled_samples))]
+        rand_ind = numpy.random.randint(low=0, high=len(list(unlabeled_samples[rand_key])))
+        img_list.append(unlabeled_samples[rand_key][rand_ind])
+    return numpy.array(img_list, dtype=float), numpy.array(img_list, dtype=float)
+#
+# def get_sample():
+#     global labeled_samples
+#     labeled_samples = labeled_samples
+#     while True:
+#         rand_key = labeled_samples.keys()[numpy.random.randint(low=0, high=len(labeled_samples))]
+#         rand_ind = numpy.random.randint(low=0, high=len(list(labeled_samples[rand_key])))
+#         yield numpy.array(labeled_samples[rand_key][rand_ind][0]).reshape(1, 2048*3), numpy.array(labeled_samples[rand_key][rand_ind][0]).reshape(1, 2048*3)
